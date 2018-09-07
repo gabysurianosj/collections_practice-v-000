c482276b4cc12e2677e9f2f0b6ec568c53acc02d
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-07 14:37:43 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -16,6 +16,10 @@ def swap_elements(array)
 end
 
 def swap_elements_from_to(array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
+  return array
+end 
 
 def reverse_array(array)
+  new_arra
   
