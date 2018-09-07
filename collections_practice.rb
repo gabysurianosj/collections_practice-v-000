90ccf1c29b171ee3e66db3bdd23b709b04a89d73
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-07 14:33:24 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,7 +3,7 @@ def sort_array_asc(array)
 end 
 
 def sort_array_desc(array)
-  array.sort {|first_num, second_num| second_num <=> first_num}
+  array.sort {|a, b| b <=> a}
 end 
 
 def sort_array_char_count(array)
